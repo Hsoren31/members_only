0c417769c4099af6e8297cc78543d0f21e90f892
Hsoren31 <sorensenhaley@gmail.com>
2025-06-23 14:52:31 -0600

    FEAT: add general styling to all pages

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.use(
   session({
     secret: process.env.SECRET,
