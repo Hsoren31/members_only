506ae743f7d6c39bff2741fcfe3fc4f1b51dd761
Hsoren31 <sorensenhaley@gmail.com>
2025-06-25 13:23:24 -0600

    FIX: update app port

--- app.js
+++ app.js
@@ -108,4 +108,5 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("app listening on port 3000!"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
